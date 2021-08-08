935f1be58ab5c2766d796e8b0b91f8477a4c70a5
gamedevjon <jon@gamedevhq.com>
2021-08-08 12:48:02 -1000

    Dev Branch Created

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneManager : NetworkBehaviour
+{
+    [SerializeField]
+    private Text _statusText;
+
+    [SerializeField]
+    private Player _player;
+}
